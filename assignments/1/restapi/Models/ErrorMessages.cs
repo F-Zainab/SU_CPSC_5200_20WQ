5e598e8fb67347042abcbf095253ba3abb01758f
F-Zainab <59784421+F-Zainab@users.noreply.github.com>
2020-01-23 14:05:56 -0800

    Update ErrorMessages.cs

--- assignments/1/restapi/Models/ErrorMessages.cs
+++ assignments/1/restapi/Models/ErrorMessages.cs
@@ -20,4 +20,11 @@ namespace restapi.Models
 
         public string Message { get => "No state transition of requested type present in timecard"; }
     }
+
+    public class InvalidApproverError
+    {
+        public int ErrorCode { get => 103; }
+
+        public string Message { get => "Approver id should be different from employee id"; }
+    }
 }
