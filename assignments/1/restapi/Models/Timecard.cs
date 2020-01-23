937caa0ff975426278f7f37c03561b686541c5af
F-Zainab <59784421+F-Zainab@users.noreply.github.com>
2020-01-23 14:08:26 -0800

    Update Timecard.cs

--- assignments/1/restapi/Models/Timecard.cs
+++ assignments/1/restapi/Models/Timecard.cs
@@ -89,9 +89,25 @@ namespace restapi.Models
                         Relationship = ActionRelationship.RecordLine,
                         Reference = $"/timesheets/{UniqueIdentifier}/lines"
                     });
+                    /////////////////Update (PATCH) a line item////////////////////////////////
 
+                    links.Add(new ActionLink()
+                    {
+                        Method = Method.Patch,
+                        Type = ContentTypes.TimesheetLine,
+                        Relationship = ActionRelationship.RecordLine,
+                        Reference = $"/timesheets/{UniqueIdentifier}/updateline/{UniqueIdentifier}"
+                    });
+
+                    links.Add(new ActionLink()
+                    {
+                        Method = Method.Post,
+                        Type = ContentTypes.Timesheet,
+                        Relationship = ActionRelationship.Create,
+                        Reference = $"~/"
+                    });
                     break;
-
+                   
                 case TimecardStatus.Submitted:
                     links.Add(new ActionLink()
                     {
@@ -116,7 +132,6 @@ namespace restapi.Models
                         Relationship = ActionRelationship.Approve,
                         Reference = $"/timesheets/{UniqueIdentifier}/approval"
                     });
-
                     break;
 
                 case TimecardStatus.Approved:
@@ -125,6 +140,13 @@ namespace restapi.Models
 
                 case TimecardStatus.Cancelled:
                     // terminal state, nothing possible here
+                    ////Added Action link for the delete as the delete method is alredy present///////////////////////////
+                    links.Add(new ActionLink()
+                    {
+                        Method = Method.Delete,
+                        Relationship = ActionRelationship.Delete,
+                        Reference = $"/timesheets/{UniqueIdentifier}"
+                    });
                     break;
             }
 
@@ -175,6 +197,27 @@ namespace restapi.Models
             Lines.Add(annotatedLine);
 
             return annotatedLine;
+        }
+
+        /*************************Replace (POST) a complete line item*******************************************/
+        public TimecardLine ReplaceLine(Guid lid, DocumentLine documentline)
+        {
+            var line = Lines.FirstOrDefault(l => l.UniqueIdentifier == lid);
+            var annotatedLine = new TimecardLine(documentline);
+            annotatedLine.UniqueIdentifier = lid;
+            Lines.Remove(line);
+            Lines.Add(annotatedLine);
+
+            return annotatedLine;
+        }
+
+        /******************Update (PATCH) a line item****************************************/
+        public TimecardLine UpdateLineItem(Guid lid, DocumentLine item)
+        {
+            var line = Lines.FirstOrDefault(l => l.UniqueIdentifier == lid);
+            line.Update(item);
+
+            return line;
         }
 
         public bool CanBeDeleted()
